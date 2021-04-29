eb0a1e1ee88cfc2a1e15690c219ff24bf3af48a3
nabesuke <nabesuke5098@gmail.com>
2021-04-29 20:02:02 +0900

    [modified]app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 const button = document.querySelector("button")!;
+
+function clickHandler(message: string) {
+  console.log("Clicked! " + message);
+}
 // comment
-button.addEventListener("click", () => {
-  console.log("clicked");
-});
+button.addEventListener("click", clickHandler.bind(null, "You're welcome"));
