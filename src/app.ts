e8f3e2dced44cfdfda0ac8c07c4fb8f971acd528
nabesuke <nabesuke5098@gmail.com>
2021-05-07 09:03:43 +0900

    arrow func.

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,9 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result = a + b;
+//   return result;
+// }
 
+const add = (a: number, b: number) => a + b;
